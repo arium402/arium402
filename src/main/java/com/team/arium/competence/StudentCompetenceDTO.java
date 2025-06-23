68e02c2dc80fa3996eeae0b65ea89d94497765d6
HappySmile52 <alice050777@naver.com>
2025-06-23 09:33:49 +0900

    핵심 역량 소개 수정 중

---
+++ src/main/java/com/team/arium/competence/StudentCompetenceDTO.java
@@ -0,0 +1,18 @@
+package com.team.arium.competence;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class StudentCompetenceDTO {
+
+	private String categoryName;	// 상위 카테고리(핵심역량)명
+	private String cclNm;	// 역량명
+	private String cclDesc;	// 역량 설명
+	
+}
